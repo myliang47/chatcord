d220d45f6b814087014c5800d06379606b81e7a8
Mella Liang <mellaliang47@gmail.com>
2020-07-17 23:03:08 -1000

    replaced complete version of public folder with template version

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const path = require('path');
+const http = require('http');
+const socketio = require('socketio');
 
 const app = express();
+const server = http.createServer( app );
+const io = socketio( server );
+
+// Set static folder
 app.use( express.static( path.join( __dirname, 'public' )));
 
+io.on( 'connection', socket => {
+  console.log('New websocket connection...');
+})
+
 const PORT = process.env.PORT || 3000;
-app.listen( PORT, () => console.log('Server is running on port', PORT));
+server.listen( PORT, () => console.log('Server is running on port', PORT));
